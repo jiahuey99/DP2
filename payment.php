79abe47930469f616a826d16eb1eaf1a275f3fbc
chunchen <chunchenjong@gmail.com>
2019-10-21 22:27:45 +0800

    Update payment.php
    show the detail order on payment

--- payment.php
+++ payment.php
@@ -76,3 +76,54 @@ $conn=mysqli_connect("localhost","root","","")
 	
 	
 </script>
+
+
+
+
+--------------------------------------------------------------------------------------------------
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Pay</title>
+</head>
+
+<body>
+	<table>
+		<tr>
+			<th>orderid</th>
+			<th>itemno</th>
+			<th>qty</th>
+			<th>idtable</th>
+			<th>total</th>
+		</tr>
+		<?php
+		$conn =mysqli_connect("localhost","root","","tabletable");
+		if($conn-> connect_error)
+		{
+			die("Connection failed:".$conn->error);
+		}
+		
+		$sql="SELECT orderid,itemno,qty,idtable,subtotal from orderdb";
+		$result =$conn->query($sql);
+		
+		
+		if($result-> num_rows >0)
+		{
+			while($row =$result-> fetch_assoc())
+			{
+				echo"<tr><td>".$row["orderid"]."</td><td>".$row["itemno"]."</td><td>".$row["qty"]."</td><td>".$row["idtable"]."</td><td>".$row["subtotal"]."</td></tr>";
+			}
+			echo"</table>";
+		
+		}
+		else
+		{
+			echo"0 result";
+		}
+		
+		$conn->close();
+		?>
+	</table>
+</body>
+</html>
