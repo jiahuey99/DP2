eb06f875119f0729bd2e68075adf270cdc894033
chunchen <chunchenjong@gmail.com>
2019-11-06 21:34:23 +0800

    Update index.php
    update for 2 user

--- index.php
+++ index.php
@@ -4,20 +4,21 @@
 <meta charset="utf-8">
 <title>User Login</title>
 	<style>
-		login
+		
+			#frm
 		{
-			width:360;
-			margin:50px auto;
-			font:Cambria, "Hoefler Text", "Liberation Serif", Times, "Times New Roman", "serif";
-			borde-radius:10px;
-			border=2px solid #ccc;
-			padding:10px 40px 25px;
-			margin-top:70px;
+			border:solid gray 10px;
+			width:30%;
+			border-radius:10px;
+			margin:100px auto;
+			background:white;
+			
 		}
+		
 		
 		input[type=text],input[type=password]
 		{
-			width:30%;
+			width:40%;
 			padding:10px;
 			margin-top:8px;
 			border:1px solid #ccc;
@@ -28,7 +29,7 @@
 		
 		input[type=submit]
 		{
-			width:10%;
+			width:20%;
 			border:2px solid #06f;
 			padding :10px;
 			font-size:20px;
@@ -38,15 +39,22 @@
 			
 		}
 		
+	
+		
 	</style>
 </head>
 
 <body>
-	<div class="Login">
+	<div class="Login" id="frm">
 <h1 align="center">Login</h1>
+		
 		<form action="" method="post" style="text-align:center;">
 		Username: <input type ="text" placeholder="Username" id="user" name="user"><br/><br/>
 		Password: <input type="password" placeholder="Password" id ="password" name="pass"><br/><br/>
+		User type: <select name="type"><option value="-1">Select user type</option>
+			<option value="Admin">Admin</option>
+			<option value="User">User</option>
+			</select><br/><br/>
 		<input type="submit" value="Login" name="submit">
 	
 		 
@@ -57,9 +65,9 @@
 	$error='';//variable to store error message
 	if(isset($_POST['submit']))
 	{
-		if(empty($_POST['user'])|| empty($_POST['pass']))
+		if(empty($_POST['user'])|| empty($_POST['pass']) || empty($_POST['type']))
 		{
-			$error="Username" or "Password is Invalid";
+			$error="Username" or "Password is Invalid" or "user type error";
 			
 		}
 		
@@ -68,20 +76,31 @@
 			// Define $user and pass	
 			$user=$_POST['user'];
 			$pass=$_POST['pass'];
+			$type=$_POST['type'];
 
 			$conn=mysqli_connect("localhost","root","");
 			//connect to database "userlogin"
 			$db=mysqli_select_db($conn,"userlogin");
 			
 			//userpass is table name(from userlogin)
-			$query=mysqli_query($conn,"SELECT *FROM userpass Where pass='$pass' AND user='$user'");
+			$query=mysqli_query($conn,"SELECT *FROM userpass Where pass='$pass' AND user='$user' AND type='$type'");
 			
-			$rows=mysqli_num_rows($query);
-			if($rows==1)
+			$row=mysqli_fetch_array($query);
+			
+				if($row['user']==$user && $row['pass']==$pass && $row['type']=='Admin')
+			{
+				//reading to other page
+				//link to next page menu
+				header("Location:staff.php");
+				
+				
+			}
+			else if($row['user']==$user && $row['pass']==$pass && $row['type']=='User')
 			{
 				//reading to other page
 				//link to next page menu
 				header("Location:menu.php");
+				
 				
 			}
 			else
@@ -89,6 +108,9 @@
 				
 				$error ="Username or Password is Invaild";
 			}
+				
+			
+			
 			//closing connection
 			mysqli_close($conn);
 			
