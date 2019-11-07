9b62a826d3a5736c0cc28bfe2a7a046d959e5278
Syongsb <48547024+Syongsb@users.noreply.github.com>
2019-11-07 17:42:44 +0800

    add navigation file

---
+++ navigation.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Order Details</title>
+<link rel="stylesheet" href="navigation.css?x={random number/string}">
+
+</head>
+
+<body>
+
+	<div class="navvv">
+	<p id="imageee"><img  src='navi.png' alt='navigation_icon' height='50px' width='50px'></p>
+	<ul id="navv">
+	  <li class="vava"><a href="menu.php">Menu</a></li>
+	  <li class="vava"><a href="order_page.php">Order Details</a></li>
+	  <li class="vava"><a href="salesreportform.php">Contact</a></li>
+	</ul>
+	</div>
+</body>
+</html>
