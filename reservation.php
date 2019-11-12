961fede3701b599b98f0458cdbd3fceee96328b9
MUHAMMAD SHAEEL ABBAS <shaeelmbrocollege@gmail.com>
2019-11-12 20:55:09 +0800

    Reservation

---
+++ reservation.php
@@ -0,0 +1,21 @@
+<?php
+	include_once 'connection.php';
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reservation</title>
+<link rel="stylesheet" href="order_page.css?b={random number/string}">
+</head>
+<body>
+<?php include'navigation.php'?>
+
+<div id=title>Reservation</div>
+</br>
+<div id=fulltable>
+
+</div>
+</body>
+</html>
