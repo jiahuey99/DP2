0f48fbca66f3b993b95347521220ce761aa33974
MUHAMMAD SHAEEL ABBAS <shaeelmbrocollege@gmail.com>
2019-11-12 20:54:51 +0800

    Reservation

--- navigation.php
+++ navigation.php
@@ -14,6 +14,7 @@
 	  <li class="vava"><a href="menu.php">Menu</a></li>
 	  <li class="vava"><a href="order_page.php">Order Details</a></li>
 	  <li class="vava"><a href="salesreportform.php">Contact</a></li>
+	  <li class="vava"><a href="reservation.php">Reservation</a></li>
 	</ul>
 	</div>
 </body>
