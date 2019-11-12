7d8270b8e774cf9d8c9a187dfc806cd76bcbf22e
jiahuey99 <56101930+jiahuey99@users.noreply.github.com>
2019-11-13 04:18:42 +0800

    CSS part3

--- navigation_admin.php
+++ navigation_admin.php
@@ -9,8 +9,8 @@
 <div class="navvv">
 	<p id="imageee"><img  src='navi.png' alt='navigation_icon' height='50px' width='50px'></p>
 	<ul id="navv">
-	  <li class="vava"><a href="menu1.php">New Order</a></li>
-	  <li class="vava"><a href="order_page.php">Order Details</a></li>
+	  <li class="vava"><a href="addmenu.php">Add new Item</a></li>
+	  <li class="vava"><a href="editmenu.php">Edit / Remove Item</a></li>
 	  
 	</ul>
 	</div>
