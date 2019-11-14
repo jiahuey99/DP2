593810cfe593b858a53556d92a73f4c71beae336
MUHAMMAD SHAEEL ABBAS <shaeelmbrocollege@gmail.com>
2019-11-14 17:20:37 +0800

    Status of tables

--- tablestatus.php
+++ tablestatus.php
@@ -18,5 +18,34 @@
 
 <div id=title>Table Status</div>
 
+<?php
+		$conn=mysqli_connect("localhost","root","","tabletable");
+			if($conn->connect_error){
+				die("Connection failed:".$conn->connect_error);
+            }
+            $reserved = "Reserved";
+            echo"<table> <tr> <th> Table ID </th> <th> Date </th> <th> Time </th> <th> Status </th> </tr>";
+            
+           $sqlres = mysqli_query($conn,"SELECT idtable,date,time FROM reservation"); 
+            while($rowres = $sqlres->fetch_assoc())
+            {
+                echo "<tr> <td>".$rowres['idtable']."</td> <td>".$rowres['date']."</td><td>".$rowres['time']."</td><td>".$reserved."</td></tr>";
+            }
+            echo"</table>";
+
+            $occupied = "Occupied";
+            echo"</br>
+
+            <table><tr> <th> Table ID </th> <th> Status </th> </tr>";
+            
+            $sqlocc = mysqli_query($conn,"SELECT DISTINCT idtable FROM orderdb");
+            while($rowocc = $sqlocc->fetch_assoc())
+            {
+                echo "<tr> <td>".$rowocc['idtable']."</td> <td>".$occupied."</td></tr>";
+            }
+
+            $sqlfree = mysqli_query($conn,"SELECT idtable FROM tableid WHERE NOT EXISTS(SELECT o.idtable FROM ORDERDB AS o ON o.orderid = r.idTABLE");
+?>
+</table>
 </body>
 </html>
