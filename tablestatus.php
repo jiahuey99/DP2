b20d8c0dfc3dd60479248af0ed85b82649497693
MUHAMMAD SHAEEL ABBAS <shaeelmbrocollege@gmail.com>
2019-11-14 15:38:43 +0800

    Table Status

---
+++ tablestatus.php
@@ -0,0 +1,22 @@
+<?php
+	include_once 'connection.php';
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reservation</title>
+<link rel="stylesheet" href="order_page.css?b={random number/string}">
+<link rel="stylesheet" href="menu1.css?c={random number/string}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+
+<?php include'navigation.php'?>
+
+<div id=title>Table Status</div>
+
+</body>
+</html>
